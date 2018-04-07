5ac3988346f3a58276d57c0d0decc55aac6f0346
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2018-04-07 19:57:12 +0800

    citations

--- extractAuthors.py
+++ extractAuthors.py
@@ -9,7 +9,6 @@ else:
 papers = json.load(open('data('+str(confidenceLimit)+').json'))
 
 authors = {}
-citationsByYear = {}
 
 for paper in [p for p in papers if 'authors' in p]:
     for author in paper['authors']:
@@ -30,16 +29,39 @@ for paper in [p for p in papers if 'authors' in p]:
 with open('authors('+str(confidenceLimit)+').json', 'w') as outfile:
     json.dump(authors, outfile)
 
-for paper in papers:
+
+
+
+citationsByYear = {}
+
+for paper in [p for p in papers if 'citations' in p and len(p['citations']) > 0]:
     for citation in paper['citations']:
-        if citation not in citationsByYear:
-            citationsByYear[citation] = {}
-            citationsByYear[citation][paper['year']] = 1
+        if isinstance(citation['title'], basestring) and citation['title'] not in citationsByYear:
+            citationsByYear[citation['title']] = {}
+            citationsByYear[citation['title']][paper['year']] = 1
         else:
-            if paper['year'] not in citationsByYear[paper['year']]:
-                citationsByYear[citation][paper['year']] = 1
-            else:
-                citationsByYear[citation][paper['year']] = citationsByYear[citation][paper['year']] + 1
+            if isinstance(citation['title'], basestring):
+                if paper['year'] not in citationsByYear[citation['title']]:
+                    citationsByYear[citation['title']][paper['year']] = 1
+                else:
+                    citationsByYear[citation['title']][paper['year']] = citationsByYear[citation['title']][paper['year']] + 1
 
 with open('citationByYear('+str(confidenceLimit)+').json', 'w') as outfile:
     json.dump(citationsByYear, outfile)
+
+citationByConference = {}
+
+for paper in [p for p in papers if 'citations' in p and len(p['citations']) > 0]:
+    for citation in paper['citations']:
+        if isinstance(citation['title'], basestring) and citation['title'] not in citationByConference:
+            citationByConference[citation['title']] = {}
+            citationByConference[citation['title']][paper['conference'][0:1]] = 1
+        else:
+            if isinstance(citation['title'], basestring):
+                if paper['conference'][0:1] not in citationByConference[citation['title']]:
+                    citationByConference[citation['title']][paper['conference'][0:1]] = 1
+                else:
+                    citationByConference[citation['title']][paper['conference'][0:1]] = citationByConference[citation['title']][paper['conference'][0:1]] + 1
+
+with open('citationByConference('+str(confidenceLimit)+').json', 'w') as outfile:
+    json.dump(citationByConference, outfile)
--- test.py
+++ test.py
@@ -1,2 +1,5 @@
 test = ['1','2','3']
 print test[-1:][0]
+
+con = 'A00'
+print con[0:1]
